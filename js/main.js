0f45662fcaaace4f0c797ee6b108095483aebea8
adam hardie <adam.hardie13@gmail.com>
2020-12-04 13:22:09 +0000

    added FAQ section, starting toggle work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log('main');
+const toggle = document.getElementsByClassName("toggle-title");
+
+toggle.addEventListener("click", () => {
+    let toggleInfo = document.getElementsByClassName('toggle-info');
+    toggleInfo.classList.add("visible-toggle");
+    
+})
