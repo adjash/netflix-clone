8eddb8578c3ba7bfb4a099aeca4bacaebef94441
adam hardie <adam.hardie13@gmail.com>
2020-12-04 14:12:22 +0000

    faq section completed with basic css transitions, will get to js at later date.

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.getElementsByClassName("toggle-title");
-
-toggle.addEventListener("click", () => {
-    let toggleInfo = document.getElementsByClassName('toggle-info');
-    toggleInfo.classList.add("visible-toggle");
-    
-})
